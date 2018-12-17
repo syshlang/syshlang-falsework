73dbc65456508942ec495e454f13cedf1fff3a77
syshlang <sunyslang@gmail.com>
2018-12-17 19:57:10 +0800

    init

---
+++ uwb-api/src/main/java/com/grgbanking/uwb/api/ApiService.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018. GRGBanking
+ * @File: ApiService.java
+ * @Description:
+ * @Author: sunys
+ * @Date: 18-12-17 下午1:36
+ * @since:
+ */
+
+package com.grgbanking.uwb.api;
+
+/**
+ * Created by sunys on 2018/12/17.
+ */
+public interface ApiService {
+}
